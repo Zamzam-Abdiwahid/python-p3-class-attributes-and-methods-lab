70aa26bd4c8a16e3d3a56cf924391866ebffb52c
Zamzam Abdiwahid <zamzamabdiwahid123@gmail.com>
2024-07-25 17:24:34 +0300

    Use class attributes and methods to write durable and powerful code

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,50 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = []
+    genres = set()
+    artists = set()
+
+    def __init__(self, title, artist, genre):
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -13,12 +13,11 @@ class TestSong:
     Song("Halo", "Beyonce", "Pop")
     Song("Smells Like Teen Spirit", "Nirvana", "Rock")
 
-    def test_saves_name_artist_genre(self):
-        '''instantiates with a name, artist, and genre.'''
-        out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
-        assert(out_of_touch.name == "Out of Touch")
-        assert(out_of_touch.artist == "Hall and Oates")
-        assert(out_of_touch.genre == "Pop")
+    def test_saves_name_artist_genre(self):'''instantiates with a name, artist, and genre.'''
+    out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
+    assert out_of_touch.title == "Out of Touch"
+    assert out_of_touch.artist == "Hall and Oates"
+    assert out_of_touch.genre == "Pop"
 
     def test_has_song_count(self):
         '''counts the total number of Song objects.'''
